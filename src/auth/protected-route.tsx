0440941a1b3b6952b452e0613e2ac1b444276fc6
io-sono-roy <110167285+io-sono-roy@users.noreply.github.com>
2023-04-30 15:29:16 +0800

    used hook for user token

--- src/auth/protected-route.tsx
+++ src/auth/protected-route.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { useUser } from "../app/hooks/useUser";
 
 
 const ProtectedRoutes = () => {
-    const user = localStorage.getItem("authToken");
+    // const user = localStorage.getItem("authToken");
+    const user = useUser();
     return (
         user ? <Outlet /> : <Navigate to="/login" />
     );
